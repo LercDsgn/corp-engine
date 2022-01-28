d8ed730db01d5cc7ba5e6f9b384b7eb8b40140e1
LercDsgn <75680333+LercDsgn@users.noreply.github.com>
2022-01-28 19:31:51 +0300

    Update __init__.py

--- corpengine1/__init__.py
+++ corpengine1/__init__.py
@@ -64,6 +64,7 @@ class Constants:
         self.ENGINEVERSION: str = '1.1.1'
         self.DEFAULTSCREENSIZE: tuple = (640, 360)
         self.WINDOWTITLE: str = 'CORP Engine window'
+        self.WINDOWICON: str = loadImage()
         self.FLAGS: int
         self.RUNNING: bool = True
         self.FPS_CAP: int = 60
@@ -100,11 +101,11 @@ class GameObject(object):
         if not hasattr(self, object.name):
             setattr(self, object.name, object)
 
-    def getGameService(self) -> object:
-        game = self.parent
-        while game.type != 'GameService':
-            game = game.parent
-        return game
+    def getService(self, serviceName) -> object: # self-service, unintented pun :)
+        service = self.parent
+        while service.type != f'{service}Service':
+            service = service.parent
+        return service
 
     def getEngine(self) -> object:
         engine = self.parent
@@ -1217,7 +1218,7 @@ def Rectangle(x: float, y: float, width: float, height: float) -> pygame.Rect:
 class Window(object):
     def __init__(self, parent: object) -> None:
         pygame.display.set_caption(constants.WINDOWTITLE)
-
+        pygame.display.set_icon(constants.WINDOWICON)
         self.parent: object = parent
         self.screen: pygame.Surface = pygame.display.set_mode(constants.DEFAULTSCREENSIZE, constants.FLAGS, 32)
         self.renderWindow: pygame.Surface = self.screen.copy()
